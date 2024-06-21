4e51bd61943e6a1960f2d7a570dfd14b10ec4431
Sam Willis <sam.willis@gmail.com>
2024-06-21 20:15:41 +0100

    Incremental live queries

--- packages/pglite/src/live/index.ts
+++ packages/pglite/src/live/index.ts
@@ -4,6 +4,12 @@ import type {
   Results,
   Transaction,
 } from "../interface";
+import type {
+  LiveNamespace,
+  LiveQueryReturn,
+  LiveChangesReturn,
+  Change,
+} from "./interface";
 
 // Counter use to generate unique IDs for live queries
 // This is used to create temporary views and so are scoped to the current connection
@@ -12,28 +18,6 @@ let liveQueryCounter = 0;
 // The notify triggers are only ever added and never removed
 // Keep track of which triggers have been added to avoid adding them multiple times
 const tableNotifyTriggersAdded = new Set<string>();
-
-interface LiveNamespace {
-  /**
-   * Create a live query
-   * @param query - The query to run
-   * @param params - The parameters to pass to the query
-   * @param callback - A callback to run when the query is updated
-   * @returns A promise that resolves to an object with the initial results,
-   * an unsubscribe function, and a refresh function
-   */
-  query<T>(
-    query: string,
-    params?: any[],
-    callback?: (results: Results<T>) => void
-  ): Promise<LiveQueryReturn<T>>;
-}
-
-interface LiveQueryReturn<T> {
-  initialResults: Results<T>;
-  unsubscribe: () => Promise<void>;
-  refresh: () => Promise<void>;
-}
 
 const setup = async (pg: PGliteInterface, emscriptenOpts: any) => {
   const namespaceObj: LiveNamespace = {
@@ -58,13 +42,19 @@ const setup = async (pg: PGliteInterface, emscriptenOpts: any) => {
         tables = await getTablesForView(tx, `live_query_${id}_view`);
         await addNotifyTriggersToTables(tx, tables);
 
+        // Create prepared statement to get the results
+        await tx.exec(`
+          PREPARE live_query_${id}_get AS
+          SELECT * FROM live_query_${id}_view;
+        `);
+
         // Get the initial results
-        results = await tx.query<T>(`SELECT * FROM live_query_${id}_view`);
+        results = await tx.query<T>(`EXECUTE live_query_${id}_get;`);
       });
 
       // Function to refresh the query
       const refresh = async () => {
-        results = await pg.query<T>(`SELECT * FROM live_query_${id}_view`);
+        results = await pg.query<T>(`EXECUTE live_query_${id}_get;`);
         callback(results);
       };
 
@@ -85,7 +75,10 @@ const setup = async (pg: PGliteInterface, emscriptenOpts: any) => {
         for (const unsub of unsubList) {
           await unsub();
         }
-        await pg.exec(`DROP VIEW IF EXISTS live_query_${id}_view`);
+        await pg.exec(`
+          DROP VIEW IF EXISTS live_query_${id}_view;
+          DEALLOCATE live_query_${id}_get;
+        `);
       };
 
       // Run the callback with the initial results
@@ -96,7 +89,272 @@ const setup = async (pg: PGliteInterface, emscriptenOpts: any) => {
         initialResults: results!,
         unsubscribe,
         refresh,
+      } satisfies LiveQueryReturn<T>;
+    },
+
+    async changes<T>(
+      query: string,
+      params: any[] | undefined | null,
+      key: string,
+      callback: (changes: Array<Change<T>>) => void
+    ) {
+      const id = liveQueryCounter++;
+      let tables: { table_name: string; schema_name: string }[];
+      let stateSwitch: 1 | 2 = 1;
+      let changes: Results<Change<T>>;
+
+      await pg.transaction(async (tx) => {
+        // Create a temporary view with the query
+        await tx.query(
+          `CREATE OR REPLACE TEMP VIEW live_query_${id}_view AS ${query}`,
+          params ?? []
+        );
+
+        // Get the tables used in the view and add triggers to notify when they change
+        tables = await getTablesForView(tx, `live_query_${id}_view`);
+        await addNotifyTriggersToTables(tx, tables);
+
+        // Get the columns of the view
+        const columns = [
+          ...(
+            await tx.query<any>(`
+              SELECT column_name, data_type 
+              FROM information_schema.columns 
+              WHERE table_name = 'live_query_${id}_view'
+            `)
+          ).rows,
+          { column_name: "__after__", data_type: "integer" },
+        ];
+
+        // Init state tables as empty temp table
+        await tx.exec(`
+          CREATE TEMP TABLE live_query_${id}_state1 (LIKE live_query_${id}_view INCLUDING ALL);
+          CREATE TEMP TABLE live_query_${id}_state2 (LIKE live_query_${id}_view INCLUDING ALL);
+        `);
+
+        // Create Diff views and prepared statements
+        for (const curr of [1, 2]) {
+          const prev = curr === 1 ? 2 : 1;
+          await tx.exec(`
+            PREPARE live_query_${id}_diff${curr} AS
+            WITH
+              prev AS (SELECT LAG("${key}") OVER () as __after__, * FROM live_query_${id}_state${prev}),
+              curr AS (SELECT LAG("${key}") OVER () as __after__, * FROM live_query_${id}_state${curr}),
+              data_diff AS (
+                -- INSERT operations: Include all columns
+                SELECT 
+                  'INSERT' AS __op__,
+                  ${columns
+                    .map(
+                      ({ column_name }) =>
+                        `curr."${column_name}" AS "${column_name}"`
+                    )
+                    .join(",\n")},
+                  ARRAY[]::text[] AS __changed_columns__
+                FROM curr
+                LEFT JOIN prev ON curr.${key} = prev.${key}
+                WHERE prev.${key} IS NULL
+              UNION ALL
+                -- DELETE operations: Include only the primary key
+                SELECT 
+                  'DELETE' AS __op__,
+                  ${columns
+                    .map(({ column_name, data_type }) => {
+                      if (column_name === key) {
+                        return `prev."${column_name}" AS "${column_name}"`;
+                      } else {
+                        return `NULL::${data_type} AS "${column_name}"`;
+                      }
+                    })
+                    .join(",\n")},
+                    ARRAY[]::text[] AS __changed_columns__
+                FROM prev
+                LEFT JOIN curr ON prev.${key} = curr.${key}
+                WHERE curr.${key} IS NULL
+              UNION ALL
+                -- UPDATE operations: Include only changed columns
+                SELECT 
+                  'UPDATE' AS __op__,
+                  ${columns
+                    .map(({ column_name, data_type }) =>
+                      column_name === key
+                        ? `curr."${column_name}" AS "${column_name}"`
+                        : `CASE 
+                            WHEN curr."${column_name}" IS DISTINCT FROM prev."${column_name}" 
+                            THEN curr."${column_name}"
+                            ELSE NULL::${data_type} 
+                            END AS "${column_name}"`
+                    )
+                    .join(",\n")},
+                    ARRAY(SELECT unnest FROM unnest(ARRAY[${columns
+                      .filter(({ column_name }) => column_name !== key)
+                      .map(
+                        ({ column_name }) =>
+                          `CASE
+                            WHEN curr."${column_name}" IS DISTINCT FROM prev."${column_name}" 
+                            THEN '${column_name}' 
+                            ELSE NULL 
+                            END`
+                      )
+                      .join(
+                        ", "
+                      )}]) WHERE unnest IS NOT NULL) AS __changed_columns__
+                FROM curr
+                INNER JOIN prev ON curr.${key} = prev.${key}
+                WHERE NOT (curr IS NOT DISTINCT FROM prev)
+              )
+            SELECT * FROM data_diff;
+          `);
+        }
+      });
+
+      const refresh = async () => {
+        await pg.transaction(async (tx) => {
+          // Populate the state table
+          await tx.exec(`
+            DELETE FROM live_query_${id}_state${stateSwitch};
+            INSERT INTO live_query_${id}_state${stateSwitch} 
+              SELECT * FROM live_query_${id}_view;
+          `);
+
+          // Get the changes
+          changes = await tx.query<any>(
+            `EXECUTE live_query_${id}_diff${stateSwitch};`
+          );
+        });
+
+        // Switch state
+        stateSwitch = stateSwitch === 1 ? 2 : 1;
+
+        callback(changes!.rows);
       };
+
+      // Setup the listeners
+      const unsubList: Array<() => Promise<void>> = [];
+      for (const table of tables!) {
+        const unsub = await pg.listen(
+          `table_change__${table.schema_name}__${table.table_name}`,
+          async () => {
+            refresh();
+          }
+        );
+        unsubList.push(unsub);
+      }
+
+      // Function to unsubscribe from the query
+      const unsubscribe = async () => {
+        for (const unsub of unsubList) {
+          await unsub();
+        }
+        await pg.exec(`
+          DROP VIEW IF EXISTS live_query_${id}_view;
+          DROP TABLE IF EXISTS live_query_${id}_state1;
+          DROP TABLE IF EXISTS live_query_${id}_state2;
+          DEALLOCATE live_query_${id}_diff1;
+          DEALLOCATE live_query_${id}_diff2;
+        `);
+      };
+
+      // Run the callback with the initial changes
+      await refresh();
+
+      // Fields
+      const fields = changes!.fields.filter(
+        (field) =>
+          !["__after__", "__op__", "__changed_columns__"].includes(field.name)
+      );
+
+      // Return the initial results
+      return {
+        fields,
+        initialChanges: changes!.rows,
+        unsubscribe,
+        refresh,
+      } satisfies LiveChangesReturn<T>;
+    },
+
+    async incrementalQuery<T>(
+      query: string,
+      params: any[] | undefined | null,
+      key: string,
+      callback: (results: Results<Change<T>>) => void
+    ) {
+      const rowsMap: Map<any, any> = new Map();
+      const afterMap: Map<any, any> = new Map();
+      let lastRows: Change<T>[] = [];
+      let firstRun = true;
+
+      const { fields, unsubscribe, refresh } = await namespaceObj.changes<T>(
+        query,
+        params,
+        key,
+        (changes) => {
+          // Process the changes
+          for (const change of changes) {
+            const {
+              __op__: op,
+              __changed_columns__: changedColumns,
+              ...obj
+            } = change as typeof change & { [key: string]: any };
+            switch (op) {
+              case "INSERT":
+                rowsMap.set(obj[key], obj);
+                afterMap.set(obj.__after__, obj[key]);
+                break;
+              case "DELETE":
+                rowsMap.delete(obj[key]);
+                afterMap.delete(obj.__after__);
+                break;
+              case "UPDATE":
+                const newObj = { ...(rowsMap.get(obj[key]) ?? {}) };
+                for (const columnName of changedColumns) {
+                  newObj[columnName] = obj[columnName];
+                  if (columnName === "__after__") {
+                    afterMap.set(obj.__after__, obj[key]);
+                  }
+                }
+                break;
+            }
+          }
+
+          // Get the rows in order
+          const rows: Change<T>[] = [];
+          let lastKey: any = null;
+          while (true) {
+            const nextKey = afterMap.get(lastKey);
+            const obj = rowsMap.get(nextKey);
+            if (!obj) {
+              break;
+            }
+            rows.push(obj);
+            lastKey = nextKey;
+          }
+          lastRows = rows;
+
+          // Run the callback
+          if (!firstRun) {
+            callback({
+              rows,
+              fields,
+            });
+          }
+        }
+      );
+
+      firstRun = false;
+      callback({
+        rows: lastRows,
+        fields,
+      });
+
+      return {
+        initialResults: {
+          rows: lastRows,
+          fields,
+        },
+        unsubscribe,
+        refresh,
+      } satisfies LiveQueryReturn<T>;
     },
   };
 
@@ -154,8 +412,11 @@ async function addNotifyTriggersToTables(
   tables: { table_name: string; schema_name: string }[]
 ) {
   const triggers = tables
-    .filter((table) =>
-      tableNotifyTriggersAdded.has(`${table.schema_name}_${table.table_name}`)
+    .filter(
+      (table) =>
+        !tableNotifyTriggersAdded.has(
+          `${table.schema_name}_${table.table_name}`
+        )
     )
     .map((table) => {
       return `
@@ -171,7 +432,9 @@ async function addNotifyTriggersToTables(
       `;
     })
     .join("\n");
-  await tx.exec(triggers);
+  if (triggers.trim() !== "") {
+    await tx.exec(triggers);
+  }
   tables.map((table) =>
     tableNotifyTriggersAdded.add(`${table.schema_name}_${table.table_name}`)
   );
---
+++ packages/pglite/src/live/interface.ts
@@ -0,0 +1,80 @@
+import type { Results } from "../interface";
+
+export interface LiveNamespace {
+  /**
+   * Create a live query
+   * @param query - The query to run
+   * @param params - The parameters to pass to the query
+   * @param callback - A callback to run when the query is updated
+   * @returns A promise that resolves to an object with the initial results,
+   * an unsubscribe function, and a refresh function
+   */
+  query<T = { [key: string]: any }>(
+    query: string,
+    params: any[] | undefined | null,
+    callback: (results: Results<T>) => void,
+  ): Promise<LiveQueryReturn<T>>;
+
+  /**
+   * Create a live query that returns the changes to the query results
+   * @param query - The query to run
+   * @param params - The parameters to pass to the query
+   * @param callback - A callback to run when the query is updated
+   * @returns A promise that resolves to an object with the initial changes,
+   * an unsubscribe function, and a refresh function
+   */
+  changes<T = { [key: string]: any }>(
+    query: string,
+    params: any[] | undefined | null,
+    key: string,
+    callback: (changes: Array<Change<T>>) => void,
+  ): Promise<LiveChangesReturn<T>>;
+
+  /**
+   * Create a live query with incremental updates
+   * @param query - The query to run
+   * @param params - The parameters to pass to the query
+   * @param callback - A callback to run when the query is updated
+   * @returns A promise that resolves to an object with the initial results,
+   * an unsubscribe function, and a refresh function
+   */
+  incrementalQuery<T = { [key: string]: any }>(
+    query: string,
+    params: any[] | undefined | null,
+    key: string,
+    callback: (results: Results<Change<T>>) => void,
+  ): Promise<LiveQueryReturn<Change<T>>>;
+}
+
+export interface LiveQueryReturn<T> {
+  initialResults: Results<T>;
+  unsubscribe: () => Promise<void>;
+  refresh: () => Promise<void>;
+}
+
+export interface LiveChangesReturn<T = { [key: string]: any }> {
+  fields: { name: string; dataTypeID: number }[];
+  initialChanges: Array<Change<T>>;
+  unsubscribe: () => Promise<void>;
+  refresh: () => Promise<void>;
+}
+
+export type ChangeInsert<T> = {
+  __changed_columns__: string[];
+  __op__: "INSERT";
+  __after__: number;
+} & T;
+
+export type ChangeDelete<T> = {} & {
+  __changed_columns__: string[];
+  __op__: "DELETE";
+  __after__: undefined;
+} & T;
+
+export type ChangeUpdate<T> = {} & {
+  __changed_columns__: string[];
+  __op__: "UPDATE";
+  __after__: number;
+} & T;
+
+export type Change<T> = ChangeInsert<T> | ChangeDelete<T> | ChangeUpdate<T>;
