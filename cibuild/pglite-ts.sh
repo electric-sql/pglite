faba13467c7e06fc51b6abb2e01a6b0d47fbda89
msfstef <msfstef@gmail.com>
2024-07-09 10:27:07 +0300

    Use pnpm everywhere

--- cibuild/pglite-ts.sh
+++ cibuild/pglite-ts.sh
@@ -20,7 +20,7 @@
 END
 
 
-    npm install
+    pnpm install
 
     mkdir -p $PGLITE/release
     rm $PGLITE/release/* 2>/dev/null
@@ -69,9 +69,9 @@ END
     # part from here
     if $CI
     then
-        npm run build:js
+        pnpm run build:js
         mkdir -p /tmp/sdk
-        npm pack
+        pnpm pack
         packed=$(echo -n electric-sql-pglite-*.tgz)
         mv $packed /tmp/sdk/pg${PGVERSION}-${packed}
 
