7a4ad5b257598a5108d5b232693c38d03f11a5c5
Sam Willis <sam.willis@gmail.com>
2024-07-30 16:14:51 +0100

    amcheck

---
+++ packages/pglite/src/contrib/amcheck.ts
@@ -0,0 +1,16 @@
+import type {
+  Extension,
+  ExtensionSetupResult,
+  PGliteInterface,
+} from "../interface";
+
+const setup = async (pg: PGliteInterface, emscriptenOpts: any) => {
+  return {
+    bundlePath: new URL("../../release/amcheck.tar.gz", import.meta.url),
+  } satisfies ExtensionSetupResult;
+};
+
+export const amcheck = {
+  name: "amcheck",
+  setup,
+} satisfies Extension;
---
+++ packages/pglite/tests/contrib/amcheck.js
@@ -0,0 +1,76 @@
+import test from "ava";
+import { PGlite } from "../../dist/index.js";
+import { amcheck } from "../../dist/contrib/amcheck.js";
+
+test("amcheck", async (t) => {
+  const pg = new PGlite({
+    extensions: {
+      amcheck,
+    },
+  });
+
+  await pg.exec("CREATE EXTENSION IF NOT EXISTS amcheck;");
+
+  // Example query from https://www.postgresql.org/docs/current/amcheck.html
+  const res = await pg.query(`
+    SELECT bt_index_check(index => c.oid, heapallindexed => i.indisunique),
+               c.relname,
+               c.relpages
+    FROM pg_index i
+    JOIN pg_opclass op ON i.indclass[0] = op.oid
+    JOIN pg_am am ON op.opcmethod = am.oid
+    JOIN pg_class c ON i.indexrelid = c.oid
+    JOIN pg_namespace n ON c.relnamespace = n.oid
+    WHERE am.amname = 'btree' AND n.nspname = 'pg_catalog'
+    -- Don't check temp tables, which may be from another session:
+    AND c.relpersistence != 't'
+    -- Function may throw an error when this is omitted:
+    AND c.relkind = 'i' AND i.indisready AND i.indisvalid
+    ORDER BY c.relpages DESC LIMIT 10;
+  `);
+
+  t.deepEqual(res.rows, [
+    {
+      bt_index_check: "",
+      relname: "pg_proc_proname_args_nsp_index",
+      relpages: 32,
+    },
+    {
+      bt_index_check: "",
+      relname: "pg_description_o_c_o_index",
+      relpages: 24,
+    },
+    {
+      bt_index_check: "",
+      relname: "pg_attribute_relid_attnam_index",
+      relpages: 16,
+    },
+    { bt_index_check: "", relname: "pg_proc_oid_index", relpages: 11 },
+    {
+      bt_index_check: "",
+      relname: "pg_attribute_relid_attnum_index",
+      relpages: 11,
+    },
+    {
+      bt_index_check: "",
+      relname: "pg_depend_depender_index",
+      relpages: 9,
+    },
+    {
+      bt_index_check: "",
+      relname: "pg_depend_reference_index",
+      relpages: 8,
+    },
+    { bt_index_check: "", relname: "pg_amop_opr_fam_index", relpages: 6 },
+    {
+      bt_index_check: "",
+      relname: "pg_amop_fam_strat_index",
+      relpages: 6,
+    },
+    {
+      bt_index_check: "",
+      relname: "pg_operator_oprname_l_r_n_index",
+      relpages: 6,
+    },
+  ]);
+});
