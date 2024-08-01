c328b194ef7057f6867d58796b84760bb2c085b5
Sam Willis <sam.willis@gmail.com>
2024-08-01 12:04:30 +0100

    contrib/uuid_ossp

---
+++ packages/pglite/src/contrib/uuid_ossp.ts
@@ -0,0 +1,16 @@
+import type {
+  Extension,
+  ExtensionSetupResult,
+  PGliteInterface,
+} from "../interface";
+
+const setup = async (pg: PGliteInterface, emscriptenOpts: any) => {
+  return {
+    bundlePath: new URL("../../release/uuid-ossp.tar.gz", import.meta.url),
+  } satisfies ExtensionSetupResult;
+};
+
+export const uuid_ossp = {
+  name: "uuid-ossp",
+  setup,
+} satisfies Extension;
