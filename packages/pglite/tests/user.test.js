8c8cd2f1730a22428495815a583f4655ff2a8600
pmp-p <mail.peny@free.fr>
2024-08-06 00:37:03 +0200

    test role switching

--- packages/pglite/tests/user.test.js
+++ packages/pglite/tests/user.test.js
@@ -6,7 +6,12 @@ test("user switching", async (t) => {
   await fs.rm("./pgdata-test-user", { force: true, recursive: true });
 
   const db = new PGlite('./pgdata-test-user');
-  await db.exec("CREATE USER test_user WITH PASSWORD 'test_password';");
+  await db.exec("CREATE USER test_user WITH PASSWORD 'md5abdbecd56d5fbd2cdaee3d0fa9e4f434';");
+
+  //
+  // CREATE DATABASE test_user WITH OWNER = test_user;
+  // CREATE SCHEMA test_user;
+  // CREATE TABLE test_user.test (
   await db.exec(`
     CREATE TABLE test (
       id SERIAL PRIMARY KEY,
@@ -14,6 +19,8 @@ test("user switching", async (t) => {
     );
     INSERT INTO test (number) VALUES (42);
   `);
+  // ALTER TABLE test_user.test OWNER TO test_user;
+  await db.exec("ALTER TABLE test OWNER TO test_user;");
   await db.close();
 
   const db2 = new PGlite({
@@ -21,7 +28,7 @@ test("user switching", async (t) => {
     username: "test_user",
   });
 
-  const test_user = await db2.query("SET ROLE test_user");
+  const test_user = await db2.exec("SET ROLE test_user");
 
   const result = await db2.query("SELECT * FROM test;");
   t.deepEqual(result.rows, [{ id: 1, number: 42 }]);
