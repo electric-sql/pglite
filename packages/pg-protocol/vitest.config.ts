75139b82fbacf89018764df9e7ec86c78d543f2b
msfstef <msfstef@gmail.com>
2024-08-22 15:13:16 +0300

    Rename package to plain pg-protocol

--- packages/pg-protocol/vitest.config.ts
+++ packages/pg-protocol/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    name: 'pglite-protocol',
+    name: 'pg-protocol',
     dir: './test',
     watch: false,
     typecheck: { enabled: true },
