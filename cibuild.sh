28ad3f74433c5fdae348e8e388f764c243725958
pmp-p <mail.peny@free.fr>
2024-08-26 12:09:44 +0200

    paths

--- cibuild.sh
+++ cibuild.sh
@@ -212,8 +212,8 @@ END
 END
     fi
 
-    mkdir -p ${PGROOT}/include/postgresql ${PGROOT}/include/postgresql/server
-    cp ${PG_DEBUG_HEADER}
+    mkdir -p ${PGROOT}/include/postgresql/server
+    cp ${PG_DEBUG_HEADER} ${PGROOT}/include/
     cp ${PG_DEBUG_HEADER} ${PGROOT}/include/postgresql
     cp ${PG_DEBUG_HEADER} ${PGROOT}/include/postgresql/server
 
