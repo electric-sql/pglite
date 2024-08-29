3ee5e608dae171b9d9ba7b24e7c972f5606af5d5
Stefanos Mousafeiris <msfstef@gmail.com>
2024-08-29 13:02:17 +0300

    fix: Add `dataDir` string arg to `create` factory method (#275)
    * Add string arg to `.create` factory
    
    * Add changeset

--- packages/pglite/vitest.config.ts
+++ packages/pglite/vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     typecheck: { enabled: true },
     testTimeout: 30000,
     hookTimeout: 30000,
-    include: ['**/*.{test,test.web}.js'],
+    include: ['**/*.{test,test.web}.{js,ts}'],
     server: {
       deps: {
         external: [/\/tests\/targets\/web\//],
