6563596de5561c17f0d97d79504ce9984ac02cf0
Sam Willis <sam.willis@gmail.com>
2024-09-26 08:51:40 +0100

    Test case for drop db and unclean shutdown

--- packages/pglite/tests/drop-database.test.ts
+++ packages/pglite/tests/drop-database.test.ts
@@ -41,4 +41,42 @@ describe('drop database', () => {
 
     expect(ret.rows).toEqual([{ id: 1, name: 'test' }])
   })
+
+  it('should drop postgres db and restart after unclean shutdown', async () => {
+    await fs.rm('./pgdata-test-drop-db2', { force: true, recursive: true })
+    {
+      let pg: PGlite | null = await PGlite.create('./pgdata-test-drop-db2')
+      await pg.exec(`
+        CREATE TABLE IF NOT EXISTS test (
+          id SERIAL PRIMARY KEY,
+          name TEXT
+        );
+      `)
+      await pg.exec("INSERT INTO test (name) VALUES ('test');")
+
+      await pg.exec(`
+        DROP DATABASE postgres;
+      `)
+
+      await pg.exec(`
+        CREATE DATABASE postgres TEMPLATE template1;
+      `)
+
+      // we don't close pg here on purpose
+      pg = null
+    }
+
+    // pause for a bit for GC...
+    await new Promise((resolve) => setTimeout(resolve, 10))
+
+    const pg2 = await PGlite.create('./pgdata-test-drop-db2', {
+      database: 'postgres',
+    })
+
+    const ret = await pg2.query(`
+      SELECT * FROM test;
+    `)
+
+    expect(ret.rows).toEqual([{ id: 1, name: 'test' }])
+  })
 })
