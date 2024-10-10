22daaef00609043f26f31772290dcd03d00a6585
Sam Willis <sam.willis@gmail.com>
2024-10-10 12:44:28 +0100

    Add PGLite REPL modal

---
+++ demos/linearlite/src/components/PGliteConsoleModal.tsx
@@ -0,0 +1,22 @@
+import { Repl } from '@electric-sql/pglite-repl'
+import Modal from './Modal'
+
+interface Props {
+  isOpen: boolean
+  onDismiss?: () => void
+}
+
+export default function PGliteConsoleModal({ isOpen, onDismiss }: Props) {
+  return (
+    <Modal
+      title="PGlite Console"
+      isOpen={isOpen}
+      onDismiss={onDismiss}
+      size="large"
+    >
+      <div className="flex flex-col w-full h-100">
+        <Repl />
+      </div>
+    </Modal>
+  )
+}
