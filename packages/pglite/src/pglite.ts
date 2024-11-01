676ee870c9e51cce941b58c7bf242a119a7f23da
Paul m. p. Peny <mail.peny@free.fr>
2024-11-01 18:17:04 +0100

    where preload ends

--- packages/pglite/src/pglite.ts
+++ packages/pglite/src/pglite.ts
@@ -368,12 +368,18 @@ export class PGlite
 
     // Initialize the database
     const idb = this.mod._pg_initdb()
-
+    
     if (!idb) {
       // This would be a sab worker crash before pg_initdb can be called
       throw new Error('INITDB failed to return value')
     }
 
+    // preloading is finished
+    // NOTE: if shm init was not in plugin constructor we may have to call 
+    // process_shared_preload_libraries after filling shared_preload_libraries_string
+    // from here because right now postgres.conf exists (if initdb did not fail ofc).
+    this.mod.HEAPU8[this.mod._process_shared_preload_libraries_in_progress] = 0
+    
     // initdb states:
     // - populating pgdata
     // - reconnect a previous db
