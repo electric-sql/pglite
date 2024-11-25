e2172d2779557912dfddb42bc5f0453fbe59b32a
User <user@pp.local>
2024-11-25 09:51:26 +0100

    style

--- packages/pglite/tests/exec-protocol.test.ts
+++ packages/pglite/tests/exec-protocol.test.ts
@@ -39,7 +39,11 @@ describe('exec protocol', () => {
 
     const r4 = await db.execProtocol(serialize.execute({}))
     const messageNames4 = r4.messages.map((msg) => msg.name)
-    expect(messageNames4).toEqual(['dataRow', 'commandComplete', 'readyForQuery'])
+    expect(messageNames4).toEqual([
+      'dataRow',
+      'commandComplete',
+      'readyForQuery',
+    ])
 
     const r5 = await db.execProtocol(serialize.sync())
     const messageNames5 = r5.messages.map((msg) => msg.name)
