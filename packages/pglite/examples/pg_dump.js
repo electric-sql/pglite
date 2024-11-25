5c1731e3fd26faf5594ef91d37476d9c1312d9f0
Sam Willis <sam.willis@gmail.com>
2024-11-25 19:48:37 +0000

    Node example

---
+++ packages/pglite/examples/pg_dump.js
@@ -0,0 +1,20 @@
+import { PGlite } from "../dist/index.js";
+import { pgDump } from "../../pglite-tools/dist/pg_dump.mjs";
+
+console.log("Starting...");
+
+const pg = await PGlite.create({ debug: 1 });
+
+// console.log("Creating table...");
+// await pg.exec(`
+//   CREATE TABLE IF NOT EXISTS test (
+//     id SERIAL PRIMARY KEY,
+//     name TEXT
+//   );
+// `);
+
+// await pg.exec("INSERT INTO test (name) VALUES ('test');");
+
+console.log('Dumping database...')
+const dump = await pgDump({ pg })
+console.log(dump)
