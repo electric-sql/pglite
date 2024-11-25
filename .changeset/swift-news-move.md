ae36974168aad6d6b38f2e9bea60e30e7a3f30df
Paul m. p. Peny <mail.peny@free.fr>
2024-11-25 10:18:19 +0100

    fix: pipelining prepared statements (#417)
    * fix socket pipelining read
    
    * one rfq after Parse, none for bind/descr/exec and one after each sync
    
    * match psycopg3 wire pipeline expectations
    
    * follow ppg3
    
    * follow ppg3
    
    * style
    
    * Chageset
    
    ---------
    
    Co-authored-by: User <user@pp.local>
    Co-authored-by: Sam Willis <sam.willis@gmail.com>

---
+++ .changeset/swift-news-move.md
@@ -0,0 +1,5 @@
+---
+'@electric-sql/pglite': patch
+---
+
+Fix a bug with pipelining prepared statements.
--- packages/pglite/tests/exec-protocol.test.ts
+++ packages/pglite/tests/exec-protocol.test.ts
@@ -27,19 +27,23 @@ describe('exec protocol', () => {
   it('should perform an extended query', async () => {
     const r1 = await db.execProtocol(serialize.parse({ text: 'SELECT $1' }))
     const messageNames1 = r1.messages.map((msg) => msg.name)
-    expect(messageNames1).toEqual(['notice', 'parseComplete'])
+    expect(messageNames1).toEqual(['notice', 'parseComplete', 'readyForQuery'])
 
     const r2 = await db.execProtocol(serialize.bind({ values: ['1'] }))
     const messageNames2 = r2.messages.map((msg) => msg.name)
-    expect(messageNames2).toEqual(['notice', 'bindComplete'])
+    expect(messageNames2).toEqual(['notice', 'bindComplete', 'readyForQuery'])
 
     const r3 = await db.execProtocol(serialize.describe({ type: 'P' }))
     const messageNames3 = r3.messages.map((msg) => msg.name)
-    expect(messageNames3).toEqual(['rowDescription'])
+    expect(messageNames3).toEqual(['rowDescription', 'readyForQuery'])
 
     const r4 = await db.execProtocol(serialize.execute({}))
     const messageNames4 = r4.messages.map((msg) => msg.name)
-    expect(messageNames4).toEqual(['dataRow', 'commandComplete'])
+    expect(messageNames4).toEqual([
+      'dataRow',
+      'commandComplete',
+      'readyForQuery',
+    ])
 
     const r5 = await db.execProtocol(serialize.sync())
     const messageNames5 = r5.messages.map((msg) => msg.name)
