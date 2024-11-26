7552fe36bc03f0cf0c3a67591663142639125636
Sam Willis <sam.willis@gmail.com>
2024-11-26 12:51:48 +0000

    Add tests for pg_dump

--- packages/pglite-tools/vitest.config.ts
+++ packages/pglite-tools/vitest.config.ts
@@ -2,17 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    name: 'pglite',
-    dir: './tests',
-    watch: false,
-    typecheck: { enabled: true },
-    testTimeout: 30000,
-    hookTimeout: 30000,
-    include: ['**/*.{test,test.web}.{js,ts}'],
-    server: {
-      deps: {
-        external: [/\/tests\/targets\/web\//],
-      },
-    },
+    globals: true,
+    environment: 'node',
   },
 })
