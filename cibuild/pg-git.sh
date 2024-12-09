562e6fde3b379897b577ee78c3b8fb2dc0721c20
pmp-p <mail.peny@free.fr>
2024-12-09 13:47:08 +0100

    test build against upstreaming 16x repo

--- cibuild/pg-git.sh
+++ cibuild/pg-git.sh
@@ -2,23 +2,22 @@ if [ -f postgresql/postgresql-${PG_VERSION}.patched ]
 then
     echo version already selected and patch stage already done
 else
-    git clone --no-tags --depth 1 --single-branch --branch master https://github.com/postgres/postgres postgresql-${PG_VERSION}
+    git clone --no-tags --depth 1 --single-branch --branch REL_16_STABLE https://github.com/electric-sql/postgres-pglite postgresql-${PG_VERSION}
 
     if pushd postgresql-${PG_VERSION}
     then
             echo
         > ./src/template/emscripten
-        > ./src/template/wasi
-        > ./src/include/port/wasi.h
-        > ./src/makefiles/Makefile.wasi
+        > ./src/include/port/emscripten.h
+        > ./src/makefiles/Makefile.emscripten
         for patchdir in \
-            postgresql-debug postgresql-wasi \
-            postgresql-wasm postgresql-wasm-${PG_VERSION} \
-            postgresql-pglite postgresql-pglite-${PG_VERSION}
+            postgresql-debug \
+            postgresql-emscripten \
+            postgresql-pglite \
         do
             if [ -d ../patches/$patchdir ]
             then
-                cat ../patches/$patchdir/*.diff | patch -p1 || exit 24
+                cat ../patches/$patchdir/*.diff | patch -p1 || exit 20
             fi
         done
         touch postgresql-${PG_VERSION}.patched
@@ -41,32 +40,8 @@ then
 else
     if $WASI
     then
-
-    CNF="${PGSRC}/configure --prefix=${PGROOT} \
- --disable-spinlocks --disable-atomics \
- --without-zlib --disable-largefile --without-llvm \
- --without-pam --disable-largefile --without-zlib --with-openssl=no \
- --without-readline --without-icu \
- ${PGDEBUG}"
-
-#  --cache-file=${PGROOT}/config.cache.wasi
-    if \
-     ZIC=/usr/sbin/zic \
-     CC=wasi-c \
-     CXX=wasi-c++ \
-     CONFIG_SITE=${PGDATA}/config.site \
-     $CNF \
-     --host=$(arch) --target=wasm32-unknown-wasi --with-template=wasi
-    then
-        echo configure ok
-        make -j 1
-    else
-        echo configure failed
-        exit 57
-    fi
-
-        read
-
+        #
+        echo "see pglite-build CI"
     else
         echo "Building $ARCHIVE (patched) from $PGSRC"
         . cibuild/pgbuild.sh
