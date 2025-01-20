67fb2aad6df1605343b3e643392fac928e869079
Sam Willis <sam.willis@gmail.com>
2025-01-20 10:14:42 +0000

    feat: add support for loading compressed dumps via loadDataDir that are not (#498)

---
+++ .changeset/witty-spiders-type.md
@@ -0,0 +1,5 @@
+---
+'@electric-sql/pglite': patch
+---
+
+feat: add support for loading compressed dumps via loadDataDir that are not labeled with mimetype
--- packages/pglite/tests/dump.test.js
+++ packages/pglite/tests/dump.test.js
@@ -78,4 +78,32 @@ describe('dump', () => {
 
     expect(ret1).toEqual(ret2)
   })
+
+  it('dump data dir and load it - compressed but not specified', async () => {
+    const pg1 = new PGlite()
+    await pg1.exec(`
+      CREATE TABLE IF NOT EXISTS test (
+        id SERIAL PRIMARY KEY,
+        name TEXT
+      );
+    `)
+    pg1.exec("INSERT INTO test (name) VALUES ('test');")
+
+    const ret1 = await pg1.query('SELECT * FROM test;')
+
+    const file = await pg1.dumpDataDir()
+
+    // remove the mime type from the file
+    const file2 = new Blob([file])
+
+    expect(typeof file2).toBe('object')
+
+    const pg2 = new PGlite({
+      loadDataDir: file2,
+    })
+
+    const ret2 = await pg2.query('SELECT * FROM test;')
+
+    expect(ret1).toEqual(ret2)
+  })
 })
