e09f982b9e2c0e25c71646a2540e35773f4af7b7
tudor <tudor@swisstch.com>
2025-02-05 18:49:56 +0100

    simple clone impl only on PGlite

---
+++ packages/pglite/tests/clone.test.js
@@ -0,0 +1,42 @@
+import { describe, it, expect } from 'vitest'
+import { PGlite } from '../dist/index.js'
+
+describe('clone', () => {
+  it('clone pglite instance', async () => {
+    const pg1 = new PGlite()
+    await pg1.exec(`
+    CREATE TABLE IF NOT EXISTS test (
+      id SERIAL PRIMARY KEY,
+      name TEXT
+    );
+  `)
+    await pg1.exec("INSERT INTO test (name) VALUES ('test');")
+    
+    const pg2 = await pg1.clone()
+    
+    const ret1 = await pg1.query('SELECT * FROM test;')
+    const ret2 = await pg2.query('SELECT * FROM test;')
+
+    expect(ret1).toEqual(ret2)
+  })
+
+  it('clone pglite instance - insert into pg2', async () => {
+    const pg1 = new PGlite()
+    await pg1.exec(`
+    CREATE TABLE IF NOT EXISTS test (
+      id SERIAL PRIMARY KEY,
+      name TEXT
+    );
+  `)
+    await pg1.exec("INSERT INTO test (name) VALUES ('test');")
+    
+    const pg2 = await pg1.clone()
+    await pg2.exec("INSERT INTO test (name) VALUES ('2-test');")
+
+    const ret1 = await pg1.query('SELECT * FROM test;')
+    const ret2 = await pg2.query('SELECT * FROM test;')
+
+    expect(ret1.rows.length).toBe(1)
+    expect(ret2.rows.length).toBe(2)
+  })
+})
