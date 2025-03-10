8d0c5b42ff357a57b91418dbf1884b6cf4838903
tudor <tudor@swisstch.com>
2025-03-10 16:31:37 +0100

    style

--- packages/pglite-tools/src/pg_dump.ts
+++ packages/pglite-tools/src/pg_dump.ts
@@ -183,7 +183,9 @@ export async function pgDump({
   })
 
   if (exitCode !== 0) {
-    throw new Error(`pg_dump failed with exit code ${exitCode}. \nError message: ${errorMessage}`)
+    throw new Error(
+      `pg_dump failed with exit code ${exitCode}. \nError message: ${errorMessage}`,
+    )
   }
 
   const file = new File(acc, fileName, {
