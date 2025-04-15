e92b54a1389089002c3046f02bf3103a719d4086
copiltembel <tudor.zaharia@gmail.com>
2025-04-15 14:08:52 +0200

    add DEBUG_INSPECT env var to pglite socket tests

--- packages/pglite-socket/tests/query-with-postgres-js.test.ts
+++ packages/pglite-socket/tests/query-with-postgres-js.test.ts
@@ -26,6 +26,7 @@ const DEBUG_TESTS_REAL_SERVER =
   process.env.DEBUG_TESTS_REAL_SERVER ||
   'postgres://postgres:postgres@localhost:5432/postgres'
 const TEST_PORT = 5434
+const DEBUG_INSPECT = process.env.DEBUG_INSPECT === "true" || undefined
 
 describe(`PGLite Socket Server`, () => {
   describe('with postgres.js client', () => {
@@ -54,6 +55,7 @@ describe(`PGLite Socket Server`, () => {
           db,
           port: TEST_PORT,
           host: '127.0.0.1',
+          inspect: DEBUG_INSPECT
         })
 
         // Add event listeners for debugging
