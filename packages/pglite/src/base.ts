71707ff970508fa1f8db6ed1d170f31194bf89e6
Tudor Zaharia <37596026+tdrz@users.noreply.github.com>
2025-05-07 21:22:09 +0200

    dataTransferContainerOverride implementation (#633)
    * feat(pglite): new dataTransferContainer option
    
    * Add _interactive_write(0) call on file path
    
    * more tests
    
    * dataTransferContainerOverride implementation
    
    * remove dev leftover line
    
    * stylecheck
    
    * Update packages/pglite/src/interface.ts
    
    Co-authored-by: Sam Willis <sam.willis@gmail.com>
    
    * Update packages/pglite/src/pglite.ts
    
    Co-authored-by: Sam Willis <sam.willis@gmail.com>
    
    * Update packages/pglite/src/worker/index.ts
    
    Co-authored-by: Sam Willis <sam.willis@gmail.com>
    
    * fixes after code review
    
    * pretty
    
    * more code review changes
    
    ---------
    
    Co-authored-by: Sam Willis <sam.willis@gmail.com>
    Co-authored-by: Paul m. p. Peny <mail.peny@free.fr>

--- packages/pglite/src/base.ts
+++ packages/pglite/src/base.ts
@@ -63,7 +63,7 @@ export abstract class BasePGlite
    */
   abstract execProtocolRaw(
     message: Uint8Array,
-    { syncToFs }: ExecProtocolOptions,
+    { syncToFs, dataTransferContainer }: ExecProtocolOptions,
   ): Promise<Uint8Array>
 
   /**
