ca9ff532869d174bf7984f3fbf2c8f82b113d549
Paul m. p. Peny <mail.peny@free.fr>
2025-05-08 16:32:49 +0200

    pglite socket + pf2/alpha (#614)
    * pf1 branch + wip pf2
    * skip one failing test to check whole CI
    LISTEN/NOTIFY does not work yet

--- packages/pglite-socket/vitest.config.ts
+++ packages/pglite-socket/vitest.config.ts
@@ -2,7 +2,15 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    name: 'integration tests',
     globals: true,
+    typecheck: { enabled: true },
     environment: 'node',
+    testTimeout: 30000,
+    watch: false,
+    dir: './tests',
+    maxWorkers: 1,
+    fileParallelism: false,
+    maxConcurrency: 1 // because we are running a TCP server on a port 
   },
 })
