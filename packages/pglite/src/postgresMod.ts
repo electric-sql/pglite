eebf961724ca3f7d3493e5062c53ac68eb4e1854
pmp-p <mail.peny@free.fr>
2025-05-20 06:16:48 +0200

    cma->socketfiles routing

--- packages/pglite/src/postgresMod.ts
+++ packages/pglite/src/postgresMod.ts
@@ -20,6 +20,7 @@ export interface PostgresMod
   preRun: Array<{ (mod: PostgresMod): void }>
   postRun: Array<{ (mod: PostgresMod): void }>
   FS: FS
+  FD_BUFFER_MAX: number
   WASM_PREFIX: string
   INITIAL_MEMORY: number
   pg_extensions: Record<string, Promise<Blob | null>>
@@ -27,6 +28,9 @@ export interface PostgresMod
   _pgl_initdb: () => number
   _pgl_backend: () => void
   _pgl_shutdown: () => void
+  _get_buffer_size: (fd: number) => number
+  _get_buffer_addr: (fd: number) => number
+  _get_channel: () => number
   _interactive_write: (msgLength: number) => void
   _interactive_one: () => void
   _interactive_read: () => number
