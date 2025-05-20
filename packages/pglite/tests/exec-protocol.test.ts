71cfb2dfaf6a72ef8ae190e93900eeac623d7fd2
pmp-p <mail.peny@free.fr>
2025-05-20 07:07:16 +0200

    style

--- packages/pglite/tests/exec-protocol.test.ts
+++ packages/pglite/tests/exec-protocol.test.ts
@@ -39,18 +39,21 @@ testDTC(async (defaultDataTransferContainer) => {
 
       const r2 = await db.execProtocol(serialize.bind({ values: ['1'] }))
       const messageNames2 = r2.messages.map((msg) => msg.name)
-      expect(messageNames2).toEqual(['notice', 'bindComplete', /*'readyForQuery'*/])
+      expect(messageNames2).toEqual([
+        'notice',
+        'bindComplete' /*'readyForQuery'*/,
+      ])
 
       const r3 = await db.execProtocol(serialize.describe({ type: 'P' }))
       const messageNames3 = r3.messages.map((msg) => msg.name)
-      expect(messageNames3).toEqual(['rowDescription', /*'readyForQuery'*/])
+      expect(messageNames3).toEqual(['rowDescription' /*'readyForQuery'*/])
 
       const r4 = await db.execProtocol(serialize.execute({}))
       const messageNames4 = r4.messages.map((msg) => msg.name)
       expect(messageNames4).toEqual([
         'dataRow',
         'commandComplete',
-/*        'readyForQuery', */
+        /*        'readyForQuery', */
       ])
 
       const r5 = await db.execProtocol(serialize.sync())
