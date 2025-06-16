eab3c40746db8142ad93fa20c516f38d5e2e0abf
tdrz <tudor.zaharia@gmail.com>
2025-06-16 08:23:24 +0200

    do not minify when doing a DEBUG build

--- examples/unixSocket/tsup.config.ts
+++ examples/unixSocket/tsup.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'tsup'
 
+const minify = process.env.DEBUG === 'true' ? false : true
+
 export default defineConfig([
   {
     entry: ['src/index.ts'],
     format: ['cjs', 'esm'],
     outDir: 'dist',
     dts: true,
-    minify: true,
+    minify: minify,
     sourcemap: true,
     clean: true,
   },
