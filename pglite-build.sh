ec63e537e38935d081063fc4ebfe26b4e74b8cd2
pmp-p <mail.peny@free.fr>
2025-07-22 05:36:49 +0200

    missing dir

--- pglite-build.sh
+++ pglite-build.sh
@@ -5,8 +5,12 @@ pushd postgres-pglite
     WASI=false ./wasm-build/build-with-docker.sh
 popd 
 
-mkdir -p ./packages/pglite/release
 
+mkdir -p ./packages/pglite-tools/release
 cp -avf postgres-pglite/dist/pg_dump.wasi ./packages/pglite-tools/release/pg_dump.wasm
+
+
+mkdir -p ./packages/pglite/release 
+
 cp -avf postgres-pglite/dist/pglite-web/. ./packages/pglite/release/
 cp -avf postgres-pglite/dist/extensions-emsdk/*.tar.gz ./packages/pglite/release/
