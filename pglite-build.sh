6898469db9502e5f7ed1840be972339b05c430c3
Paul m. p. Peny <mail.peny@free.fr>
2025-07-22 16:12:13 +0200

    pg_dump ci (#731)
    * pg_dump ci
    
    Co-authored-by: tudor <tudor.zaharia@gmail.com>

---
+++ pglite-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+pushd postgres-pglite
+	WASI=true ./wasm-build/build-with-docker.sh
+    WASI=false ./wasm-build/build-with-docker.sh
+popd 
+
+
+mkdir -p ./packages/pglite-tools/release
+cp -avf postgres-pglite/dist/pg_dump.wasi ./packages/pglite-tools/release/pg_dump.wasm
+
+
+mkdir -p ./packages/pglite/release 
+
+cp -avf postgres-pglite/dist/pglite-web/. ./packages/pglite/release/
+cp -avf postgres-pglite/dist/extensions-emsdk/*.tar.gz ./packages/pglite/release/
