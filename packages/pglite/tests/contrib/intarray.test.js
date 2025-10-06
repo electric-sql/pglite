00cdf8411ac5119c8c24eb9daa6633a419c0aebc
tudor <tudor.zaharia@gmail.com>
2025-10-06 15:38:44 +0200

    added intarray contrib extension

---
+++ packages/pglite/tests/contrib/intarray.test.js
@@ -0,0 +1,90 @@
+import { it, expect } from 'vitest'
+import { PGlite } from '../../dist/index.js'
+import { intarray } from '../../dist/contrib/intarray.js'
+
+it('intarray', async () => {
+  const pg = await PGlite.create({
+    extensions: {
+      intarray,
+    },
+  })
+
+  await pg.exec('CREATE EXTENSION IF NOT EXISTS intarray;')
+
+  await pg.exec(`
+    CREATE TABLE articles (
+      id SERIAL PRIMARY KEY,
+      title TEXT NOT NULL,
+      tag_ids INTEGER[] -- This will store an array of integer tag IDs);`)
+
+  await pg.exec(`
+    INSERT INTO articles (title, tag_ids) VALUES
+    ('Postgres Performance Tips', '{1,2,3}'),
+    ('Introduction to SQL', '{2,4}'),
+    ('Advanced intarray Usage', '{1,3,5}'),
+    ('Database Normalization', '{4,6}');`)
+
+  const titleTags25 = await pg.query(`
+    SELECT title, tag_ids
+    FROM articles
+    WHERE tag_ids && '{2,5}'::integer[];`)
+
+  expect(titleTags25.rows).toEqual([
+    {
+      title: 'Postgres Performance Tips',
+      tag_ids: [1, 2, 3],
+    },
+    {
+      title: 'Introduction to SQL',
+      tag_ids: [2, 4],
+    },
+    {
+      title: 'Advanced intarray Usage',
+      tag_ids: [1, 3, 5],
+    },
+  ])
+
+  const titleTags12 = await pg.query(`
+    SELECT title, tag_ids
+    FROM articles
+    WHERE tag_ids @> '{1,2}'::integer[];`)
+
+  expect(titleTags12.rows).toEqual([
+    {
+      title: 'Postgres Performance Tips',
+      tag_ids: [1, 2, 3],
+    },
+  ])
+
+  const titleTags1235 = await pg.query(`
+    SELECT title, tag_ids
+    FROM articles
+    WHERE tag_ids <@ '{1,2,3,5}'::integer[];`)
+
+  expect(titleTags1235.rows).toEqual([
+    {
+      title: 'Postgres Performance Tips',
+      tag_ids: [1, 2, 3],
+    },
+    {
+      title: 'Advanced intarray Usage',
+      tag_ids: [1, 3, 5],
+    },
+  ])
+
+  const queryInt = await pg.query(`
+    SELECT title, tag_ids
+    FROM articles
+    WHERE tag_ids @@ '1 & (3|4)'::query_int;`)
+
+  expect(queryInt.rows).toEqual([
+    {
+      title: 'Postgres Performance Tips',
+      tag_ids: [1, 2, 3],
+    },
+    {
+      title: 'Advanced intarray Usage',
+      tag_ids: [1, 3, 5],
+    },
+  ])
+})
