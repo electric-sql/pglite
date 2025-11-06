a42e1dd5423152c431d5d207b0ef0ee3cb76ccaa
tudor <tudor.zaharia@gmail.com>
2025-11-06 17:38:29 +0100

    docs

--- docs/extensions/extensions.data.ts
+++ docs/extensions/extensions.data.ts
@@ -532,7 +532,8 @@ const baseExtensions: Extension[] = [
     description: `
     The pgcrypto module provides cryptographic functions for PostgreSQL.
     `,
-    shortDescription: 'The pgcrypto module provides cryptographic functions for PostgreSQL.',
+    shortDescription:
+      'The pgcrypto module provides cryptographic functions for PostgreSQL.',
     docs: 'https://www.postgresql.org/docs/current/pgcrypto.html',
     tags: ['postgres extension', 'postgres/contrib'],
     importPath: '@electric-sql/pglite/contrib/pgcrypto',
