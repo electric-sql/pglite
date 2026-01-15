4878e60eea3eebe571ef0474ceb96c976e5f8343
tudor <tdrz@zaharia.ch>
2026-01-15 09:48:24 +0100

    something is working

--- packages/pglite-initdb/src/initdb.ts
+++ packages/pglite-initdb/src/initdb.ts
@@ -10,50 +10,26 @@ const initdbExePath = '/pglite/bin/initdb'
 const pgstdoutPath = '/pglite/pgstdout'
 const pgstdinPath = '/pglite/pgstdin'
 
+// "-c", "zero_damaged_pages=on"
 // "-c", "checkpoint_flush_after=1",
-// const baseArgs = [
-// "-c", "ignore_checksum_failure=on",
-// // "-c", "log_checkpoints=false",
-// // "-c", "search_path=pg_catalog",
-// // "-c", "exit_on_error=true",
-// "-c", "ignore_invalid_pages=on",
-// "-c", "zero_damaged_pages=on",
-// "-c", "ignore_system_indexes=on",
-// // "-c", "temp_buffers=8MB",
-// // "-c", "work_mem=4MB",
-// "-c", "fsync=on",
-// "-c", "synchronous_commit=on",
-// // "-c", "wal_buffers=4MB",
-// // "-c", "min_wal_size=80MB",
-// // "-c", "shared_buffers=128MB"
-// ]
+const baseArgs = [
+"-c", "ignore_checksum_failure=on",
+// "-c", "log_checkpoints=false",
+// "-c", "search_path=pg_catalog",
+// "-c", "exit_on_error=true",
+"-c", "ignore_invalid_pages=on",
+"-c", "zero_damaged_pages=on",
+"-c", "ignore_system_indexes=on",
+// "-c", "temp_buffers=8MB",
+// "-c", "work_mem=4MB",
+"-c", "fsync=on",
+"-c", "synchronous_commit=on",
+// "-c", "wal_buffers=4MB",
+// "-c", "min_wal_size=80MB",
+// "-c", "shared_buffers=128MB"
+]
 
 // const baseArgs: string[] = []
-
-const baseArgs = [
-  "-d", "4",
-  "-D", PGDATA,
-  // "-c", "exit_on_error=true",
-  // "-c", "checkpoint_flush_after=1",
-  "-c", "fsync=on", 
-  // "-c", "synchronous_commit=on",
-  // "-c", "effective_io_concurrency=1",
-  // "-c", "maintenance_io_concurrency=1",
-  // "-c", "backend_flush_after=1",
-  // "-c", "io_combine_limit=1",
-  "-c", "ignore_invalid_pages=on",
-  "-c", "ignore_system_indexes=on",
-  "-c", "ignore_checksum_failure=on",
-  // "-c", "backend_flush_after=1",
-  "-c", "zero_damaged_pages=on",
-
-  "-c", "temp_buffers=8MB",
-  "-c", "work_mem=4MB",
-  "-c", "wal_buffers=4MB",
-  "-c", "min_wal_size=80MB",
-  "-c", "shared_buffers=128MB",
-  "-c", "search_path=pg_catalog",
-]
 
 interface ExecResult {
   exitCode: number
@@ -93,34 +69,31 @@ async function execInitdb({
     // }
 
     if (args[0] === '--boot') {
-      
+
       console.log("boot")
-      args.push(...baseArgs)
-      // args = [
-      //   "--boot",
-      //   "-D", PGDATA,
-      //   "-d", "3",
-      //   ...baseArgs,
-      //   // "-r", "/dev/null",
-      //   "-X", 
-      //   "1048576"]
+      args = [
+        "--boot",
+        "-D", PGDATA,
+        "-d", "1",
+        ...baseArgs,
+        // "-r", "/dev/null",
+        "-X", 
+        "1048576"]
     }
 
     if (args[0] === '--single') {
-      // process.exit(99)
       console.log("--single")
       if (args[args.length-1] === 'template1') {
         const x = args.pop()
-        args.push(...baseArgs, "-B", "16", "-S", "512", "-f", "siobtnmh", x!)
-        // args = [
-        //   "--single",
-        //   "-d", "3",
-        //   "-B", "16", "-S", "512", "-f", "siobtnmh",
-        //   "-D", PGDATA,
-        //   "-O", "-j",
-        //   // "-r", "/dev/null",
-        //   x!
-        // ]
+        args = [
+          "--single",
+          "-d", "1",
+          "-B", "16", "-S", "512", "-f", "siobtnmh",
+          "-D", PGDATA,
+          "-O", "-j",
+          // "-r", "/dev/null",
+          x!
+        ]
       }
     }
 
@@ -134,22 +107,13 @@ async function execInitdb({
     // pg.Module.FS.writeFile(pgstdoutPath, '')
     pg.Module.HEAPU8.set(origHEAPU8)
 
-    {
-      const pglite_stdin_path = pg.Module.stringToUTF8OnStack(pgstdinPath)
-      const rmode = pg.Module.stringToUTF8OnStack('r')
-      pg.Module._pgl_freopen(pglite_stdin_path, rmode, 0)
-      const pglite_stdout_path = pg.Module.stringToUTF8OnStack(pgstdoutPath)
-      const wmode = pg.Module.stringToUTF8OnStack('w')
-      pg.Module._pgl_freopen(pglite_stdout_path, wmode, 1)
-    }    
-
-    console.error('executing pg main with', args)
+    console.log('executing pg main with', args)
     const result = pg.callMain(args)
     // pg.Module.HEAPU8.set(origHEAPU8)
     // pg.Module._pgl_proc_exit(66)
     // pg.Module.___funcs_on_exit()
     // pg.Module._fflush(0);
-    console.log("callMain result=", result)
+    console.log(result)
     // pglite_stdin_fd && pg.Module._fclose(pglite_stdin_fd)
     // pglite_stdout_fd && pg.Module._fclose(pglite_stdout_fd)
 
@@ -196,18 +160,10 @@ async function execInitdb({
 
             if (smode === 'r') {
               pgMainResult = callPgMain(postgresArgs)
-              const initdb_path = mod.stringToUTF8OnStack(pgstdoutPath)
-              const rmode = mod.stringToUTF8OnStack('r')
-              initdb_stdin_fd = mod._fopen(initdb_path, rmode)
-
               return initdb_stdin_fd;
             } else {
               if (smode === 'w') {
                 needToCallPGmain = true
-                const path = mod.stringToUTF8OnStack(pgstdinPath)
-                const wmode = mod.stringToUTF8OnStack('w')
-                initdb_stdout_fd = mod._fopen(path, wmode)
-
                 return initdb_stdout_fd;
               } else {
                 throw `Unexpected popen mode value ${smode}`
@@ -223,16 +179,9 @@ async function execInitdb({
               // if the last popen had mode w, execute now postgres' main()
               if (needToCallPGmain) {
                 needToCallPGmain = false
-                mod._fclose(stream)
                 pgMainResult = callPgMain(postgresArgs)
-              } else {
-                mod._fclose(stream)
               }
-              if (stream === initdb_stdin_fd) {
-                initdb_stdin_fd = -1
-              } else if (stream === initdb_stdout_fd) {
-                initdb_stdout_fd = -1
-              }
+              // const closeResult = mod._fclose(stream)
               // console.log(closeResult)
               return pgMainResult
             } else {
@@ -242,7 +191,26 @@ async function execInitdb({
           }, 'pi')
 
           mod._pgl_set_pclose_fn(pclose_fn)
+
+          {
+            const pglite_stdin_path = pg.Module.stringToUTF8OnStack(pgstdinPath)
+            const rmode = pg.Module.stringToUTF8OnStack('r')
+             pg.Module._pgl_freopen(pglite_stdin_path, rmode, 0)
+            const pglite_stdout_path = pg.Module.stringToUTF8OnStack(pgstdoutPath)
+            const wmode = pg.Module.stringToUTF8OnStack('w')
+             pg.Module._pgl_freopen(pglite_stdout_path, wmode, 1)
+          }
   
+          {
+            const initdb_path = mod.stringToUTF8OnStack(pgstdoutPath)
+            const rmode = mod.stringToUTF8OnStack('r')
+            initdb_stdin_fd = mod._fopen(initdb_path, rmode)
+  
+            const path = mod.stringToUTF8OnStack(pgstdinPath)
+            const wmode = mod.stringToUTF8OnStack('w')
+            initdb_stdout_fd = mod._fopen(path, wmode)
+          }
+
           // pg.Module.FS.chdir(PGDATA)
         }
       },
@@ -299,7 +267,7 @@ export async function initdb({
   const execResult = await execInitdb({
     pg,
     args: ["--wal-segsize=1", "--allow-group-access", "-E", "UTF8", "--locale=C.UTF-8", "--locale-provider=libc",
-      ...(args ?? [])],
+      ...baseArgs, ...(args ?? [])],
   })
 
   if (execResult.exitCode !== 0) {
