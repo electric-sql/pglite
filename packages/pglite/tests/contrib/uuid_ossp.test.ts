b330fc23d994b217e1f2b696b1c08fc45f62f63f
Tudor Zaharia <tdrz@zaharia.ch>
2026-05-23 08:33:08 +0200

    same as above for uuid_ossp extension tests

--- packages/pglite/tests/contrib/uuid_ossp.test.ts
+++ packages/pglite/tests/contrib/uuid_ossp.test.ts
@@ -1,31 +1,37 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, beforeEach, afterEach } from 'vitest'
 import { PGlite } from '../../dist/index.js'
 import { uuid_ossp } from '../../dist/contrib/uuid_ossp.js'
 
 describe('uuid_ossp', () => {
-  it('uuid_generate_v1', async () => {
-    const pg = new PGlite({
-      extensions: {
-        uuid_ossp,
-      },
-    })
-
+  let pg: PGlite
+  let dataDirArchive: File | Blob
+  beforeEach(async () => {
+    if (!dataDirArchive) {
+      pg = await PGlite.create({
+        extensions: { uuid_ossp },
+      })
+      dataDirArchive = await pg.dumpDataDir('gzip')
+    } else {
+      pg = await PGlite.create({
+        extensions: { uuid_ossp },
+        loadDataDir: dataDirArchive,
+      })
+    }
     await pg.exec('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
+  })
+  afterEach(async () => {
+    if (!pg.closed) {
+      await pg.close()
+    }
+  })
 
+  it('uuid_generate_v1', async () => {
     const res = await pg.query('SELECT uuid_generate_v1() as value;')
 
     expect(res.rows[0].value.length).toBe(36)
   })
 
   it('uuid_generate_v3', async () => {
-    const pg = new PGlite({
-      extensions: {
-        uuid_ossp,
-      },
-    })
-
-    await pg.exec('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
-
     const res = await pg.query(
       "SELECT uuid_generate_v3(uuid_ns_dns(), 'www.example.com') as value;",
     )
@@ -34,28 +40,12 @@ describe('uuid_ossp', () => {
   })
 
   it('uuid_generate_v4', async () => {
-    const pg = new PGlite({
-      extensions: {
-        uuid_ossp,
-      },
-    })
-
-    await pg.exec('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
-
     const res = await pg.query('SELECT uuid_generate_v4() as value;')
 
     expect(res.rows[0].value.length).toBe(36)
   })
 
   it('uuid_generate_v5', async () => {
-    const pg = new PGlite({
-      extensions: {
-        uuid_ossp,
-      },
-    })
-
-    await pg.exec('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
-
     const res = await pg.query(
       "SELECT uuid_generate_v5(uuid_ns_dns(), 'www.example.com') as value;",
     )
@@ -64,42 +54,18 @@ describe('uuid_ossp', () => {
   })
 
   it('uuid_nil', async () => {
-    const pg = new PGlite({
-      extensions: {
-        uuid_ossp,
-      },
-    })
-
-    await pg.exec('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
-
     const res = await pg.query('SELECT uuid_nil() as value;')
 
     expect(res.rows[0].value).toBe('00000000-0000-0000-0000-000000000000')
   })
 
   it('uuid_ns_dns', async () => {
-    const pg = new PGlite({
-      extensions: {
-        uuid_ossp,
-      },
-    })
-
-    await pg.exec('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
-
     const res = await pg.query('SELECT uuid_ns_dns() as value;')
 
     expect(res.rows[0].value).toBe('6ba7b810-9dad-11d1-80b4-00c04fd430c8')
   })
 
   it('uuid_ns_oid', async () => {
-    const pg = new PGlite({
-      extensions: {
-        uuid_ossp,
-      },
-    })
-
-    await pg.exec('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
-
     const res = await pg.query('SELECT uuid_ns_oid() as value;')
 
     expect(res.rows[0].value).toBe('6ba7b812-9dad-11d1-80b4-00c04fd430c8')
