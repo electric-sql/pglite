9919da57014a7058c0da3e55be598b6e3faea851
Tudor Zaharia <tdrz@zaharia.ch>
2026-05-27 15:28:16 +0200

    style

--- packages/pglite-pg_hashids/tests/pg_hashids.test.ts
+++ packages/pglite-pg_hashids/tests/pg_hashids.test.ts
@@ -2,112 +2,112 @@ import { describe, it, expect, beforeEach, afterEach } from 'vitest'
 import { PGlite } from '@electric-sql/pglite'
 import { pg_hashids } from '../src/index.js'
 
-  describe(`pg_hashids`, () => {
-    let pg: PGlite
-    let dataDirArchive: File | Blob
-    beforeEach(async () => {
-      if (!dataDirArchive) {
-        pg = await PGlite.create({
-          extensions: { pg_hashids },
-        })
-        dataDirArchive = await pg.dumpDataDir('gzip')
-      } else {
-        pg = await PGlite.create({
-          extensions: { pg_hashids },
-          loadDataDir: dataDirArchive,
-        })
-      }
-      await pg.exec('CREATE EXTENSION IF NOT EXISTS pg_hashids;')
-    })
+describe(`pg_hashids`, () => {
+  let pg: PGlite
+  let dataDirArchive: File | Blob
+  beforeEach(async () => {
+    if (!dataDirArchive) {
+      pg = await PGlite.create({
+        extensions: { pg_hashids },
+      })
+      dataDirArchive = await pg.dumpDataDir('gzip')
+    } else {
+      pg = await PGlite.create({
+        extensions: { pg_hashids },
+        loadDataDir: dataDirArchive,
+      })
+    }
+    await pg.exec('CREATE EXTENSION IF NOT EXISTS pg_hashids;')
+  })
 
-    afterEach(async () => {
-      if (!pg.closed) {
-        await pg.close()
-      }
-    })
+  afterEach(async () => {
+    if (!pg.closed) {
+      await pg.close()
+    }
+  })
 
-    it('can load extension', async () => {
-      const res = await pg.query<{ extname: string }>(`
+  it('can load extension', async () => {
+    const res = await pg.query<{ extname: string }>(`
         SELECT extname
         FROM pg_extension
         WHERE extname = 'pg_hashids'
       `)
 
-      expect(res.rows).toHaveLength(1)
-      expect(res.rows[0].extname).toBe('pg_hashids')
-    })
-
-    it('should return a hash using the default alphabet and empty salt', async () => {
-      const res = await pg.exec(`SELECT id_encode(1001);`)
-
-      expect(res[0].rows[0].id_encode).toEqual('jNl')
-    })
-
-    it('should return a hash using the default alphabet and supplied salt', async () => {
-      const res = await pg.exec(`SELECT id_encode(1234567, 'This is my salt');`)
-
-      expect(res[0].rows[0].id_encode).toEqual('Pdzxp')
-    })
-
-    it('should return a hash using the default alphabet, salt and minimum hash length', async () => {
-      const res = await pg.exec(
-        `SELECT id_encode(1234567, 'This is my salt', 10);`,
-      )
-
-      expect(res[0].rows[0].id_encode).toEqual('PlRPdzxpR7')
-    })
-
-    it('should return a hash using the supplied alphabet, salt and minimum hash length', async () => {
-      const res = await pg.exec(
-        `SELECT id_encode(1234567, 'This is my salt', 10, 'abcdefghijABCDxFGHIJ1234567890');`,
-      )
-
-      expect(res[0].rows[0].id_encode).toEqual('3GJ956J9B9')
-    })
-
-    it('should decode previously generated hash', async () => {
-      const res = await pg.exec(
-        `SELECT id_decode('PlRPdzxpR7', 'This is my salt', 10);`,
-      )
-
-      expect(res[0].rows[0].id_decode).toEqual([1234567])
-    })
-
-    it('should decode previously generated hash using the supplied alphabet', async () => {
-      const res = await pg.exec(
-        `SELECT id_decode('3GJ956J9B9', 'This is my salt', 10, 'abcdefghijABCDxFGHIJ1234567890');`,
-      )
-
-      expect(res[0].rows[0].id_decode).toEqual([1234567])
-    })
-
-    it('should decode previously generated hash into a single integer', async () => {
-      const res = await pg.exec(`SELECT id_decode_once('jNl');`)
-
-      expect(res[0].rows[0].id_decode_once).toEqual(1001)
-    })
-
-    it('should decode previously generated hash into a single integer using the supplied salt', async () => {
-      const res = await pg.exec(
-        `SELECT id_decode_once('Pdzxp', 'This is my salt');`,
-      )
-
-      expect(res[0].rows[0].id_decode_once).toEqual(1234567)
-    })
-
-    it('should decode previously generated hash into a single integer using the supplied salt and minimum hash length', async () => {
-      const res = await pg.exec(
-        `SELECT id_decode_once('PlRPdzxpR7', 'This is my salt', 10);`,
-      )
-
-      expect(res[0].rows[0].id_decode_once).toEqual(1234567)
-    })
-
-    it('should decode previously generated hash into a single integer using the supplied alphabet', async () => {
-      const res = await pg.exec(
-        `SELECT id_decode_once('3GJ956J9B9', 'This is my salt', 10, 'abcdefghijABCDxFGHIJ1234567890');`,
-      )
-
-      expect(res[0].rows[0].id_decode_once).toEqual(1234567)
-    })
+    expect(res.rows).toHaveLength(1)
+    expect(res.rows[0].extname).toBe('pg_hashids')
   })
+
+  it('should return a hash using the default alphabet and empty salt', async () => {
+    const res = await pg.exec(`SELECT id_encode(1001);`)
+
+    expect(res[0].rows[0].id_encode).toEqual('jNl')
+  })
+
+  it('should return a hash using the default alphabet and supplied salt', async () => {
+    const res = await pg.exec(`SELECT id_encode(1234567, 'This is my salt');`)
+
+    expect(res[0].rows[0].id_encode).toEqual('Pdzxp')
+  })
+
+  it('should return a hash using the default alphabet, salt and minimum hash length', async () => {
+    const res = await pg.exec(
+      `SELECT id_encode(1234567, 'This is my salt', 10);`,
+    )
+
+    expect(res[0].rows[0].id_encode).toEqual('PlRPdzxpR7')
+  })
+
+  it('should return a hash using the supplied alphabet, salt and minimum hash length', async () => {
+    const res = await pg.exec(
+      `SELECT id_encode(1234567, 'This is my salt', 10, 'abcdefghijABCDxFGHIJ1234567890');`,
+    )
+
+    expect(res[0].rows[0].id_encode).toEqual('3GJ956J9B9')
+  })
+
+  it('should decode previously generated hash', async () => {
+    const res = await pg.exec(
+      `SELECT id_decode('PlRPdzxpR7', 'This is my salt', 10);`,
+    )
+
+    expect(res[0].rows[0].id_decode).toEqual([1234567])
+  })
+
+  it('should decode previously generated hash using the supplied alphabet', async () => {
+    const res = await pg.exec(
+      `SELECT id_decode('3GJ956J9B9', 'This is my salt', 10, 'abcdefghijABCDxFGHIJ1234567890');`,
+    )
+
+    expect(res[0].rows[0].id_decode).toEqual([1234567])
+  })
+
+  it('should decode previously generated hash into a single integer', async () => {
+    const res = await pg.exec(`SELECT id_decode_once('jNl');`)
+
+    expect(res[0].rows[0].id_decode_once).toEqual(1001)
+  })
+
+  it('should decode previously generated hash into a single integer using the supplied salt', async () => {
+    const res = await pg.exec(
+      `SELECT id_decode_once('Pdzxp', 'This is my salt');`,
+    )
+
+    expect(res[0].rows[0].id_decode_once).toEqual(1234567)
+  })
+
+  it('should decode previously generated hash into a single integer using the supplied salt and minimum hash length', async () => {
+    const res = await pg.exec(
+      `SELECT id_decode_once('PlRPdzxpR7', 'This is my salt', 10);`,
+    )
+
+    expect(res[0].rows[0].id_decode_once).toEqual(1234567)
+  })
+
+  it('should decode previously generated hash into a single integer using the supplied alphabet', async () => {
+    const res = await pg.exec(
+      `SELECT id_decode_once('3GJ956J9B9', 'This is my salt', 10, 'abcdefghijABCDxFGHIJ1234567890');`,
+    )
+
+    expect(res[0].rows[0].id_decode_once).toEqual(1234567)
+  })
+})
