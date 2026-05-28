2e78ecc59ba3123523659a96ed7cf65c6ebc7ec7
Tudor Zaharia <tdrz@zaharia.ch>
2026-05-28 09:41:21 +0200

    update deno tests

--- packages/pglite/tests/targets/deno/runtest.sh
+++ packages/pglite/tests/targets/deno/runtest.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 rm -rf ./pgdata-test ./node_modules
-deno install
 mkdir -p ./node_modules/@electric-sql/pglite/dist
+mkdir -p ./node_modules/@electric-sql/pglite-pgvector/dist
 cp -Rf ../../../dist/* node_modules/@electric-sql/pglite/dist/
-TZ=UTC deno test --allow-read --allow-write --allow-env --allow-sys --node-modules-dir ./*.test.deno.js
+cp -Rf ../../../../pglite-pgvector/dist/* node_modules/@electric-sql/pglite-pgvector/dist/
+cp ../../../package.json node_modules/@electric-sql/pglite/package.json
+cp ../../../../pglite-pgvector/package.json node_modules/@electric-sql/pglite-pgvector/package.json
+TZ=UTC deno test --allow-read --allow-write --allow-env --allow-sys --node-modules-dir=manual ./*.test.deno.js
 
