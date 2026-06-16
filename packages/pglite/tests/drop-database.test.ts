2ccbb4c615072c8991fe86c958a3afbd326e6ad5
Tudor Zaharia <37596026+tdrz@users.noreply.github.com>
2026-06-16 18:01:52 +0200

    Parsing improvements (#1030)
    * depending on the caller, we might not need to parse the end results
    
    * style
    
    * fix pg_dump; do not deploy postgres_fdw.tar.gz
    
    * improve pg_dump
    
    * cleanup
    
    * less copy
    
    * changeset

--- packages/pglite/tests/drop-database.test.ts
+++ packages/pglite/tests/drop-database.test.ts
@@ -1,8 +1,13 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, afterAll } from 'vitest'
 import { PGlite } from '../dist/index.js'
 import * as fs from 'fs/promises'
 
 describe('drop database', () => {
+  afterAll(async () => {
+    await fs.rm('./pgdata-test-drop-db', { force: true, recursive: true })
+    await fs.rm('./pgdata-test-drop-db2', { force: true, recursive: true })
+  })
+
   it('should create and drop database', async () => {
     const pg = await PGlite.create()
 
